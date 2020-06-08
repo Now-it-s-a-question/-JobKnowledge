400f3e6fdf964fee556ac182498a86a0bfbca01e
Carrick96 <lt1491446341@gmail.com>
2020-06-08 15:56:37 +0800

    项目结构更新

---
+++ src/main/java/cn/edu/cqut/dao/ContactMapper.java
@@ -0,0 +1,20 @@
+package cn.edu.cqut.dao;
+
+import cn.edu.cqut.entity.Contact;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Glorious
+ * @since 2020-06-08
+ */
+public interface ContactMapper extends BaseMapper<Contact> {
+	@Select("select * from contact where cusNo=#{cusNo}")
+	List<Contact> selectContactByCusNo(String cusNo);
+}
