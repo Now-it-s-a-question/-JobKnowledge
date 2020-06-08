d96db75e5e8b898f11f1da26d177cccdcd56c87b
Carrick96 <lt1491446341@gmail.com>
2020-06-08 16:00:55 +0800

    修复了包扫描路径的问题

--- src/main/java/cn/edu/cqut/CrmserviceApplication.java
+++ src/main/java/cn/edu/cqut/CrmserviceApplication.java
@@ -4,8 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Glorious
+ */
 @SpringBootApplication
-@MapperScan("cn.edu.cqut.mapper")
+@MapperScan("cn.edu.cqut.dao")
 public class CrmserviceApplication {
 
 	public static void main(String[] args) {
